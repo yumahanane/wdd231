11fd5ab9aeed363cefb4ec199660c3f4a26c935d
yumahanane <yuma.hanane1@gmail.com>
2025-11-12 21:30:38 +0200

    json almost done

---
+++ chamber/scripts/chamber.js
@@ -0,0 +1,39 @@
+const url = 'https://yumahanane.github.io/wdd231/chamber/data/members.json';
+
+const cards = document.querySelector('#cards');
+
+async function getCompaniesData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.table(data); 
+    displayCompanies(data.companies); 
+};
+
+getCompaniesData();
+
+const displayCompanies = (companies) => { 
+    companies.forEach(company => {
+        let card = document.createElement('section');
+        let logo = document.createElement('img');
+        let address = document.createElement('p');
+        let contact = document.createElement('p');
+        let siteurl = document.createElement('p');
+
+        address.textContent = company.address;
+        contact.textContent = company.contact;
+        siteurl.textContent = company.websiteurl;
+        logo.setAttribute('src', prophet.imageurl);
+        logo.setAttribute('alt', `Logo of ${company.name}`); 
+        logo.setAttribute('loading', 'lazy');
+        logo.setAttribute('width', '150');
+        logo.setAttribute('height', 'auto');
+
+        card.appendChild(logo);
+        card.appendChild(address);
+        card.appendChild(contact);
+        card.appendChild(siteurl);
+
+        cards.appendChild(card); 
+    });
+
+}
