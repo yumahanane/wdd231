292e52d753d13973cbc00ecbecd44ff8b9a62b5e
yumahanane <yuma.hanane1@gmail.com>
2025-11-13 03:18:30 +0200

    changes in json file

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -1,18 +1,33 @@
-const url = 'https://yumahanane.github.io/wdd231/chamber/data/members.json';
 
+const gridbutton = document.querySelector('#grid');
+const listbutton = document.querySelector('#list');
 const cards = document.querySelector('#cards');
+
+const url = 'https://yumahanane.github.io/wdd231/chamber/data/members.json';
 
 async function getCompaniesData() {
     const response = await fetch(url);
     const data = await response.json();
-    displayCompanies(data.companies); 
+
+    gridbutton.addEventListener('click', () => {
+        displayCompaniesGrid(data.companies);
+    })
+
+    listbutton.addEventListener('click', () => {
+        displayCompaniesList(data.companies);
+    });
 };
 
 getCompaniesData();
 
-const displayCompanies = (companies) => { 
+
+function displayCompaniesGrid(companies) {
+    cards.innerHTML = "";
+
     companies.forEach(company => {
         let card = document.createElement('section');
+        card.setAttribute('class', 'grid-card');
+
         let logo = document.createElement('img');
         let address = document.createElement('p');
         let contact = document.createElement('p');
@@ -20,9 +35,9 @@ const displayCompanies = (companies) => {
 
         address.textContent = company.address;
         contact.textContent = company.contact;
-        siteurl.textContent = company.websiteurl;
+        siteurl.innerHTML = `${company.websiteurl}`;
         logo.setAttribute('src', company.imageurl);
-        logo.setAttribute('alt', `Logo of ${company.name}`); 
+        logo.setAttribute('alt', `Logo of ${company.name}`);
         logo.setAttribute('loading', 'lazy');
         logo.setAttribute('width', '150');
         logo.setAttribute('height', 'auto');
@@ -32,7 +47,32 @@ const displayCompanies = (companies) => {
         card.appendChild(contact);
         card.appendChild(siteurl);
 
-        cards.appendChild(card); 
+        cards.appendChild(card);
     });
+}
 
+function displayCompaniesList(companies) {
+    cards.innerHTML = "";
+
+    companies.forEach(company => {
+        let card = document.createElement('section');
+        card.setAttribute('class', 'list-card');
+
+        let name = document.createElement('p');
+        let address = document.createElement('p');
+        let contact = document.createElement('p');
+        let siteurl = document.createElement('p');
+
+        name.textContent = company.name;
+        address.textContent = company.address;
+        contact.textContent = company.contact;
+        siteurl.textContent = company.websiteurl;
+
+        card.appendChild(name);
+        card.appendChild(address);
+        card.appendChild(contact);
+        card.appendChild(siteurl);
+
+        cards.appendChild(card);
+    });
 }
