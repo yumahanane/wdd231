6af8773573f59982f9b933708cbeb02d48114538
yumahanane <yuma.hanane1@gmail.com>
2025-11-22 22:43:40 +0200

    modules done

---
+++ modules/scripts/modules.mjs
@@ -0,0 +1,20 @@
+import byuiCourse from "./course.mjs";
+import { setSectionSelection } from "./sections.mjs";
+import { setTitle, renderSections } from "./output.mjs";
+
+
+document.querySelector("#enrollStudent").addEventListener("click", function () {
+    const sectionNum = Number(document.querySelector("#sectionNumber").value);
+    byuiCourse.changeEnrollment(sectionNum);
+    renderSections(byuiCourse.sections);
+});
+document.querySelector("#dropStudent").addEventListener("click", function () {
+    const sectionNum = Number(document.querySelector("#sectionNumber").value);
+    byuiCourse.changeEnrollment(sectionNum, false);
+    renderSections(byuiCourse.sections);
+});
+
+setTitle(byuiCourse);
+setSectionSelection(byuiCourse.sections);
+renderSections(byuiCourse.sections);
+
---
+++ modules/scripts/sections.mjs
@@ -0,0 +1,15 @@
+export function setSectionSelection() {
+    const sectionSelect = document.querySelector("#sectionNumber");
+    byuiCourse.sections.forEach((section) => {
+        const option = document.createElement("option");
+        option.value = section.sectionNumber;
+        option.textContent = `${section.sectionNumber}`;
+        sectionSelect.appendChild(option);
+    });
+}
+
+
+
+
+
+
