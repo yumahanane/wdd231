7ba8efc760aeaa397603dc3797f25ccc32104151
yumahanane <yuma.hanane1@gmail.com>
2025-11-23 02:09:42 +0200

    weather html and javascript done

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,21 +1,21 @@
-const currentTemp = document.querySelector(".current-temp");
+// Select HTML elements for current weather 
+const currentTemp = document.querySelector("#current-temp");
 const weatherIcon = document.querySelector("#weather-icon");
 const figCaption = document.querySelector("figcaption");
 
-
+// Select HTML elements for weather forecast
+const currentday = document.querySelector("#today");
 const tomorrow = document.querySelector("#tomorrow");
 const aftertomorrow = document.querySelector("#dayaftertomorrow");
 
+// Define URL
 const key = `a6879f332f76c71d2de2a6391b3a219f`;
 const lat = "-19.8324";
 const long = "34.8408";
 
-
-
-
 const url = `//api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=${key}&units=metric`;
 
-
+// Fetch Data
 async function apiFetch() {
     try {
         const response = await fetch(url);
@@ -31,25 +31,26 @@ async function apiFetch() {
     }
 }
 
+// To determine days of the week
+const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const day = new Date();
 
-
+// Display data
 function displayResults(data) {
-    currentTemp.innerHTML = `${data.list[0].main.temp}&deg;C`; // &deg; is code for degree symbol
-
-    const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+    // for current weather
+    const iconsrc = `https://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png`
     weatherIcon.setAttribute('src', iconsrc);
-    weatherIcon.setAttribute('alt', data.weather[0].description);
+    weatherIcon.setAttribute('alt', data.list[0].weather[0].description);
+    currentTemp.innerHTML = `${data.list[0].main.temp}&deg;C`; // &deg; is code for degree symbol
+    figCaption.innerHTML = `${data.list[0].weather[0].description}`;
 
-    let desc = data.weather[0].description;
-    figCaption.textContent = `${desc}`;
-
-
-
-    tomorrow.innerHTML = `${data.main.temp}&deg;C`; // &deg; is code for degree symbol
-    aftertomorrow.innerHTML = `${data.main.temp}&deg;C`; // &deg; is code for degree symbol
-
+    // for weather forecast
+    currentday.innerHTML = `Today: ${data.list[0].main.temp}&deg;C`;
+    tomorrow.innerHTML = `${weekday[day.getDay() + 1]}: ${data.list[1].main.temp}&deg;C`; 
+    aftertomorrow.innerHTML = `${weekday[day.getDay() + 2]}: ${data.list[2].main.temp}&deg;C`; 
 }
 
-
 apiFetch();
+
+
 
