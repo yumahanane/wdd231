106dbe0d4a74b70feddd385ea651191f2ef0808b
yumahanane <yuma.hanane1@gmail.com>
2025-12-06 16:39:20 +0200

    small view done

--- chamber/scripts/places.mjs
+++ chamber/scripts/places.mjs
@@ -1,7 +1,6 @@
 import { areas } from '../data/areas.mjs';
 
-console.log(areas);
-
+// console.log(areas); // for testing
 
 const gridbutton = document.querySelector('#grid');
 const listbutton = document.querySelector('#list');
@@ -11,12 +10,9 @@ displayAreas(areas);
 
 cards.classList.add('grid');
 
-
 gridbutton.addEventListener('click', () => {
     cards.classList.add('grid');
     cards.classList.remove('list');
-
-
 })
 
 listbutton.addEventListener('click', () => {
@@ -36,13 +32,16 @@ function displayAreas(places) {
         let description = document.createElement('p');
         let moredetails = document.createElement('button');
 
+        card.setAttribute('id', 'acard');
         name.textContent = place.title;
         name.setAttribute('id', 'place-title');
         address.textContent = place.address;
         address.setAttribute('id', 'place-address');
         description.textContent = place.description;
+        description.setAttribute('id', 'place-description');
         moredetails.innerHTML = `${place.moreinfo}`
         moredetails.textContent = "Learn More";
+        moredetails.setAttribute('id', 'place-details');
         photo.setAttribute('src', place.imageurl);
         photo.setAttribute('alt', `Logo of ${place.title}`);
         photo.setAttribute('loading', 'lazy');
@@ -56,6 +55,26 @@ function displayAreas(places) {
         card.appendChild(moredetails);
 
         cards.appendChild(card);
+
+        moredetails.addEventListener('click', () => {
+            displayMoreDetails(place);
+        });
+    });
+}
+
+const moreDetails = document.querySelector('#dialogBox');
+
+function displayMoreDetails(place) {
+    moreDetails.innerHTML = "";
+    moreDetails.innerHTML = `
+    <button id="closeModal">❌</button>
+    <h2><strong>${place.title}</strong></h2>
+    <p>${place.moreinfo}</p>
+  `;
+    moreDetails.showModal();
+
+    closeModal.addEventListener("click", () => {
+        moreDetails.close();
     });
 }
 
